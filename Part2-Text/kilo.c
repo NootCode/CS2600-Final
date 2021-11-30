863cf39e5800d2081cfc988620baf341784e336b
Andre Nalbandian <andrenalbandian@gmail.com>
2021-11-30 13:58:03 -0800

    finished with tildes

--- Part2-Text/kilo.c
+++ Part2-Text/kilo.c
@@ -5,10 +5,15 @@
 #include <stdio.h>
 #include <errno.h>
 
+# defin CTRL_KEY(k) ((k) & 0x1f)
 //NEEDS TO BE REMOVED
 #define STDIN_FILENO
+#define STDOUT_FILENO
 
 void die(const char *s){
+    write(STDOUT_FILENO, "\x1b[2J", 4);
+    write(STDOUT_FILENO, "\x1b[H",3);
+
     perror(s);
     exit(1);
 }
@@ -34,19 +39,48 @@ void enableRawMode(){
     tcsetattr(STDIN_FILENO, TCSAFLUSH, &raw);
 }
 
+char editorReadKey(){
+    int nread;
+    char c;
+    while((nread = read(STDIN_FILENO, &c, 1)) != 1){
+        if(nread == 01 && errno != EAGAIN) 
+            die("read");
+    }
+    return c;
+}
+
+void editorProcessKeypress(){
+    char c = editorReadKey();
+    switch(c){
+        case CTRL_KEY('q'):
+            write(STDOUT_FILENO, "\x1b[2J", 4);
+            write(STDOUT_FILENO, "\x1b[H",3);
+            exit(0);
+            break;
+    }
+}
+
+void editorDrawRows(){
+    int y;
+    for(y = 0; y < 24; y++){
+        write(STDOUT_FILENO, "~\r\n" ,3);
+    }
+}
+
+void editorRefreshScreen(){
+    write(STDOUT_FILENO, "\x1b[2J", 4);
+    write(STDOUT_FILENO, "\x1b[H",3);
+
+    editorDrawRows();
+    wirte(STDOUT_FILENO, "\x1b[H" , 3);
+}
+
 int main(){
     enableRawMode();
     char c;
     while(1){
-        char c = '\0';
-        if(read(STDIN_FILENO, &c, 1) == -1 && errno != EAGAIN)
-            die("read");
-        if(iscntrl(c)){
-            printf("%d\r\n", c);
-        }else{
-            printf("%d ('%c')\r\n", c, c);
-        }
-        if(c == 'q') break;
+        editorRefreshScreen();
+        editorProcessKeypress();
     }
     return 0;
 }
