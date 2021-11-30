597ae9af1382644a3572b5dade348bd9095f31ea
Andre Nalbandian <andrenalbandian@gmail.com>
2021-11-30 13:46:52 -0800

    Chapter 2 Complete

---
+++ Part2-Text/kilo.c
@@ -0,0 +1,52 @@
+#include <unistd.h> 
+#include <temios.h>
+#include <stdlib.h>
+#include <ctype.h>
+#include <stdio.h>
+#include <errno.h>
+
+//NEEDS TO BE REMOVED
+#define STDIN_FILENO
+
+void die(const char *s){
+    perror(s);
+    exit(1);
+}
+
+struct termios orig_termios;
+void disableRawMode(){
+    if(tcsetattr(STDIN_FILENO, TCSAFLUSH, &orig_termios) == -1)
+        die("tcsetattr");
+}
+
+void enableRawMode(){
+    if(tcgetattr(STDIN_FILENO, &orig_termios) == -1) 
+        die("tcgetattr");
+    atexit(disableRawMode);
+    stuct termios raw = orig_termios;
+    raw.c_iflag &= ~(BRKINT| ICRNL| INPCK| ISTRIP | IXON);
+    raw.c_oflag &- ~(OPOST);
+    raw.c_cflag |= (CS8);
+    raw.c_lflag &= ~(ECHO | ICANON | IEXTEN | ISIG);
+
+    raw.c_cc[VMIN] = 0;
+    raw.c_cc[VTIME] = 1;
+    tcsetattr(STDIN_FILENO, TCSAFLUSH, &raw);
+}
+
+int main(){
+    enableRawMode();
+    char c;
+    while(1){
+        char c = '\0';
+        if(read(STDIN_FILENO, &c, 1) == -1 && errno != EAGAIN)
+            die("read");
+        if(iscntrl(c)){
+            printf("%d\r\n", c);
+        }else{
+            printf("%d ('%c')\r\n", c, c);
+        }
+        if(c == 'q') break;
+    }
+    return 0;
+}
