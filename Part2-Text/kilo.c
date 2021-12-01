4b966be7c1adb0c75669bbd5d235554d7248275d
Andre Nalbandian <andrenalbandian@gmail.com>
2021-12-01 00:58:25 -0800

    Chapter 3* complete

--- Part2-Text/kilo.c
+++ Part2-Text/kilo.c
@@ -1,6 +1,6 @@
 /** includes **/
 #include <unistd.h> 
-#include <temios.h>
+#include <termios.h>
 #include <stdlib.h>
 #include <ctype.h>
 #include <stdio.h>
@@ -88,6 +88,7 @@ int editorReadKey(){
                 if(seq[2] =='~'){
                     switch(seq[1]){
                         case '1': return HOME_KEY;
+                        case '3': return DEL_KEY;
                         case '4': return END_KEY;
                         case '5': return PAGE_UP;
                         case '6': return PAGE_DOWN;
